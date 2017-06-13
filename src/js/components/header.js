ab0e2bed3cd3d8ff76944d4f37850767735eddf8
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-13 14:18:37 -0400

    Add the header component to take the space

---
+++ src/js/components/header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class HeaderComponent extends React.Component {
+  render() {
+    return (
+      <header>
+      <h1>Header</h1>
+      </header>
+    )
+
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
+import HeaderComponent from './components/header.js'
 
 export default class Root extends React.Component{
   render(){
     return (
+      <div>
+      <HeaderComponent />
       <h1>Hello World</h1>
+      </div>
     );
   };
 
